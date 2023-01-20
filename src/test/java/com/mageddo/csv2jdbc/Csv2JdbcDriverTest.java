45e6e04dc22f73b0d46dc5e01b0307049532369c
Elvis de Freitas <edigitalb@gmail.com>
2023-01-20 20:05:58 -0300

    fixing test

--- src/test/java/com/mageddo/csv2jdbc/Csv2JdbcDriverTest.java
+++ src/test/java/com/mageddo/csv2jdbc/Csv2JdbcDriverTest.java
@@ -121,28 +121,36 @@ class Csv2JdbcDriverTest {
   }
 
   @Test
-  void mustExtractQueryToCsv() {
+  void mustExtractQueryToCsv(@TempDir Path tempDir) throws Exception {
 
     // arrange
     final var jdbi = Jdbi.create(JDBC_URL, "SA", "");
+    final var csvFile = tempDir.resolve("csv.csv");
 
     // act
     // assert
     jdbi.useHandle(h -> {
       final var updated = h
-          .createUpdate("""
+          .createUpdate(String.format("""
               CSV2J COPY (
                 SELECT 1 AS ID, 10.99 AS AMOUNT, TIMESTAMP '2022-01-31 23:59:58.987' AS DAT_CREATION
                 UNION ALL
                 SELECT 2 AS ID, 7.50 AS AMOUNT, TIMESTAMP '2023-01-31 21:59:58.987' AS DAT_CREATION
-              ) TO '/Users/elfreitas/Documents/test.csv' WITH CSV HEADER
+              ) TO '%s' WITH CSV HEADER
               """
-          )
+          , csvFile))
           .execute();
       assertEquals(2, updated);
     });
 
-    // todo assert file content
+    assertEquals(
+        """
+            ID,AMOUNT,DAT_CREATION
+            1,10.99,2022-01-31 23:59:58.987
+            2,7.50,2023-01-31 21:59:58.987
+            """.replaceAll("\n", "\r\n"),
+        Files.readString(csvFile)
+    );
 
   }
 
